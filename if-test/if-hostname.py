3b32fa6cf658fc592d2eb262df2427a435562321
Ching-Yi Wang <ching-yi.wang@verizonwireless.com>
2018-11-06 16:16:58 -0500

    if-test 2nd try message

---
+++ if-test/if-hostname.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+hostname = input('Please enter a hostname: ')
+
+if hostname.lower() == 'mtg':
+  print('The hostname was found to be mtg')
+  print('hostname matches expected config')
+else:
+  print('Exiting the script')
+  exit()
+
+
+
---
+++ lab_input/input_ip.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+user_input = input('Please enter an IPv4 IP address:')
+print("You told me the IPv4 address is:" + user_input)
+device_input = input('Please enter the vendor name assosicate with the device:')
+print("The vendor names associated with the device are:" + device_input)
+
